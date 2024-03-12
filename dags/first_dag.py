55ae8985ce87134d1a2d3acc2c8da5999e82adf3
Samogonn <solovevym@gmail.com>
2024-03-12 10:33:27 +0300

    task dependancies

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -10,7 +10,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id="first_dag",
+    dag_id="first_dag_v5",
     default_args=default_args,
     description="This is my first dag",
     start_date=datetime(2024, 3, 7),
@@ -21,4 +21,20 @@ with DAG(
         bash_command="echo hello world, this is the first task",
     )
 
-    task1
+    task2 = BashOperator(
+        task_id="second_task",
+        bash_command="echo This is second task and will be running after task1",
+    )
+
+    task3 = BashOperator(
+        task_id="third_task",
+        bash_command="echo This is third task and will be running after task1",
+    )
+
+    # task1.set_downstream(task2)
+    # task1.set_downstream(task3)
+
+    # task1 >> task2
+    # task1 >> task3
+
+    task1 >> [task2, task3]
